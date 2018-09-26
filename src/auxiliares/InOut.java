d999a3fbb0657975d966ccbd00d15eee6e62b651
Adriano Laureano <sl4ureano@outlook.com>
2018-09-26 16:13:50 -0300

    Update InOut.java

--- src/auxiliares/InOut.java
+++ src/auxiliares/InOut.java
@@ -1,20 +1,25 @@
 package auxiliares;
+
 import javax.swing.JOptionPane;
 
 public class InOut {
-	
+
 	public static final String arquivo = "contas.txt";
-	
-	public static int InInt(String titulo){
-		int in = Integer.parseInt(JOptionPane.showInputDialog(titulo));		
+
+	public static int InInt(String titulo) {
+		try {
+			int in = Integer.parseInt(JOptionPane.showInputDialog(titulo));
+			return in;
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+
+	public static float InFloat(String titulo) {
+		float in = Float.parseFloat(JOptionPane.showInputDialog(titulo));
 		return in;
 	}
-	
-	public static float InFloat(String titulo){
-		float in = Float.parseFloat(JOptionPane.showInputDialog(titulo));		
-		return in;
-	}
-	
+
 	public static double InDouble(String titulo) {
 		try {
 			double in = Double.parseDouble(JOptionPane.showInputDialog(titulo));
@@ -24,18 +29,18 @@ public class InOut {
 		}
 
 	}
-	
-	public static String InString(String titulo){
-		String in = JOptionPane.showInputDialog(titulo);		
+
+	public static String InString(String titulo) {
+		String in = JOptionPane.showInputDialog(titulo);
 		return in;
 	}
-	
-	public static void  OutMessage(String msg, String titulo) {
+
+	public static void OutMessage(String msg, String titulo) {
 		JOptionPane.showMessageDialog(null, msg, titulo, JOptionPane.INFORMATION_MESSAGE);
 	}
-	
-	public static void  OutMessage(String msg) {
+
+	public static void OutMessage(String msg) {
 		JOptionPane.showMessageDialog(null, msg);
 	}
-	
+
 }
